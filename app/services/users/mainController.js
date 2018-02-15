a121209679802bba9ec7c452f549830611a7c96e
jimeej <35932841+jimeej@users.noreply.github.com>
2018-02-15 09:04:03 +0530

    controller file.

---
+++ app/services/users/mainController.js
@@ -0,0 +1,5 @@
+var app = angular.module('MyApp', []);
+    app.controller('myctrl', [function () {
+        var vm = this;
+        vm.mode = 'fun'; //Lets test if property name is set to Rahil
+    }]);
